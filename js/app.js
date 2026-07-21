67067f9638aa36e2e1dda44bfb5c8e04599dc39a
cherosli33-hub <cherosli33@gmail.com>
2026-07-21 13:25:51 +0800

    Kemas kini item dan kuantiti checklist PHC

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 export const CATEGORIES = [
   { id:"airway", name:"Airway & Ventilation", icon:"◉", items:[
-    ["Nasal cannula (adult)",3],["Nasal cannula (paed)",3],["High flow mask (adult)",3],["High flow mask (paed)",3],["Nebulizer mask (adult)",3],["Nebulizer mask (paed)",3],["Oropharyngeal airway",5],["Nasopharyngeal airway",2],["Bag Valve Mask (adult)",1],["Bag Valve Mask (paed)",1],["BVM tubing",1],["BVM mask (adult)",2],["BVM mask (paed)",2],["Endotracheal tube size 7.0mm",2],["Endotracheal tube size 7.5mm",2],["Endotracheal tube size 8.0mm",2],["Endotracheal tube size 5.0mm",2],["Endotracheal tube size 5.5mm",2],["Endotracheal tube size 6.0mm",2],["Stylet (adult)",1],["Stylet (paed)",1],["Laryngeal mask airway size 2",2],["Laryngeal mask airway size 3",2],["Laryngeal mask airway size 4",2],["Laryngoscope handle",1],["Laryngoscope blade size 2",1],["Laryngoscope blade size 3",1],["Laryngoscope blade size 4",1],["Lubrication gel",1],["Anchor tape",1],["10cc Syringe",2]
+    ["Nasal cannula (adult)",2],["Nasal cannula (paed)",2],["High flow mask (adult)",2],["High flow mask (paed)",2],["Nebulizer mask (adult)",2],["Nebulizer mask (paed)",2],["Oropharyngeal airway",5],["Nasopharyngeal airway",2],["Bag Valve Mask (adult)",1],["Bag Valve Mask (paed)",1],["BVM tubing",1],["Endotracheal tube size 7.0mm",1],["Endotracheal tube size 6.5mm",1],["Endotracheal tube size 7.5mm",2],["Endotracheal tube size 8.0mm",2],["Endotracheal tube size 5.0mm",2],["Endotracheal tube size 5.5mm",2],["Endotracheal tube size 6.0mm",1],["Stylet (adult)",1],["Stylet (paed)",1],["Laryngeal mask airway size 2",1],["Laryngeal mask airway size 3",1],["Laryngeal mask airway size 4",1],["Laryngoscope handle",1],["Laryngoscope blade size 2",1],["Laryngoscope blade size 3",1],["Laryngoscope blade size 4",1],["Lubrication gel",1],["Anchor tape",1],["10cc Syringe",2]
   ]},
   { id:"iv", name:"IV Access", icon:"✚", items:[
     ["Branula size 16G",4],["Branula size 18G",4],["Branula size 20G",4],["Branula size 22G",4],["Tegaderm",5],["Alcohol swab",20],["Stopper",2],["3cc Syringe",4],["5cc Syringe",4],["10cc Syringe",4],["Needle size 21G",3],["Needle size 23G",3]
@@ -9,16 +9,16 @@ export const CATEGORIES = [
     ["Dextrose 50%",5],["Tab. Aspirin",5],["Sublingual GTN (bottle)",1],["Atropine 1mg",3],["Adrenaline 1mg",5],["Tramadol 50mg",4],["Diclofenac sodium 75mg",2],["Chlorpheniramine Maleate 10mg",2],["Metoclopramide 10mg",2],["Hydrocortisone 100mg",4],["Haloperidol 10mg",2],["Combivent solution",5],["Atrovent solution",5],["Salbutamol solution (bottle)",1],["Water for Injection",5],["Heparin saline",5]
   ]},
   { id:"drip", name:"IV Drip", icon:"滴", items:[
-    ["Normal saline 0.9%",1],["Dextrose 10%",1],["Drip set tubing (adult)",1],["Microdrip chamber (paed)",1]
+    ["Normal saline 0.9%",1],["Dextrose 10%",1],["Drip set tubing (adult)",1]
   ]},
   { id:"dxt", name:"Dextrostix Kit", icon:"DXT", phc1Only:true, items:[
     ["Dextrostix machine",1],["Dextrostix strips (bottle)",1],["Lancet",10]
   ]},
   { id:"wound", name:"Wound Management", icon:"✥", items:[
-    ["Adhesive tape / micropore (roll)",1],["Gauze pack",5],["Cotton pack",5],["Gamgee pack",3],["Arm sling",4],["Crepe bandage 10cm (roll)",3],["Crepe bandage 6cm (roll)",3],["Compression bandage",2],["Haemostatic bandage",2],["Commercial tourniquet",1],["Burnshield dressing kit",1]
+    ["Adhesive tape / micropore (roll)",1],["Gauze pack",5],["Cotton pack",5],["Gamgee pack",3],["Arm sling",4],["Crepe bandage 10cm (roll)",3],["Crepe bandage 6cm (roll)",3],["Commercial tourniquet",1]
   ]},
   { id:"personal", name:"Personal Equipment", icon:"▣", items:[
-    ["Rescue scissor",1],["Torchlight",1],["Stethoscope",1],["Sterile glove",3],["Chest leads",15],["Spider strapping",1],["Goggles",3]
+    ["Rescue scissor",1],["Torchlight",1],["Stethoscope",1],["Sterile glove",3],["Chest leads",15],["Spider strapping",1]
   ]}
 ];
 
