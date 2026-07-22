6d18e4d4059bb3031932e5a76d75d8fc6627b1ba
cherosli33-hub <cherosli33@gmail.com>
2026-07-23 01:43:48 +0800

    Refresh shared data every five seconds

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.5.6";
+export const APP_VERSION = "2.5.7";
 
 // Isi URL deployment Google Apps Script selepas Sheet produksi dipasang
 // dalam akaun Google pemilik sistem.
