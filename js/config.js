2f723a2175802b6fbf54ca291db55bedf8f1b6ff
cherosli33-hub <cherosli33@gmail.com>
2026-07-23 01:26:58 +0800

    Refresh dashboard across devices

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.5.2";
+export const APP_VERSION = "2.5.3";
 
 // Isi URL deployment Google Apps Script selepas Sheet produksi dipasang
 // dalam akaun Google pemilik sistem.
